297a4b7c82e36d150f824253114890552b4bc279
Lateef Taiwo <taiwolateef55@gmail.com>
2024-02-24 01:56:50 +0100

    Added backend.tf and modified README.md

---
+++ backend.tf
@@ -0,0 +1,38 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "savvy-dev-terraform-bucket"
+  # Enable versioning so we can see the full revision history of our state files
+  versioning {
+    enabled = true
+  }
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "yournamehere"
+
+#   # ... other configuration ...
+# }
+
+# resource "aws_s3_bucket_versioning" "terraform_state" {
+#   bucket = aws_s3_bucket.terraform_state.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
